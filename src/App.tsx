637b64dfcb2d500925bf4428a19065db5bf8b136
Muhammad Auzair <78294230+Nobody12here@users.noreply.github.com>
2024-03-09 19:47:15 +0500

    Optimized the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Header from './components/Header/Header';
 import StakingCard from './components/StakingCard/StakingCard';
-import WithdrawDetails from './components/WithdrawDetails/WithdrawDetails';
 import './App.css'
 
 function App() {
--- src/components/StakingCard/StakingCard.tsx
+++ src/components/StakingCard/StakingCard.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, FC } from "react";
+import {  FC } from "react";
 import { useState } from "react";
 import "./StakingCard.style.css";
 import "./InputForm.style.css";
