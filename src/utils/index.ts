948e310824245ea1f526641b4b4c0b08b5e19ad4
Muhammad Auzair <78294230+Nobody12here@users.noreply.github.com>
2024-03-11 21:21:18 +0500

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,67 +1,7 @@
-import { Config } from "wagmi";
-import { WriteContractMutateAsync } from "wagmi/query";
-import { token_abi,token_address,contract_abi,contract_address } from "../contract";
-import { parseUnits,formatEther } from "viem";
-import { useWaitForTransactionReceipt } from "wagmi";
+
+import { formatEther } from "viem";
 
 
-async function handleSubmit(
-  farmError: any,
-  approveTokens: WriteContractMutateAsync<Config, unknown>,
-  farmTokens: WriteContractMutateAsync<Config, unknown>,
-  tokenAmount: number,
-  selectedDuration: number | null,
-  selectedPackage: "unlock" | "locked"
-) {
-  if (selectedPackage === "locked") {
-    if (selectedDuration) {
-      //First approve the tokens
-      const hash = await approveTokens({
-        abi: token_abi,
-        address: token_address,
-        functionName: "approve",
-        args: [contract_address, parseUnits(tokenAmount.toString(), 18)], // Remove the unnecessary BigInt conversion
-      });
-      const { data } = useWaitForTransactionReceipt({hash});
-      console.log(data)
-      
-    try {
-      await farmTokens({
-        abi: contract_abi,
-        address: contract_address,
-        functionName: "farm",
-        args: [parseUnits(tokenAmount.toString(), 18), selectedDuration],
-      });
-      alert("Staked successfully");
-    } catch (error) {
-      alert(error)
-    }
-  } else {
-    console.log("Please select a duration");
-  }
-} else if (selectedPackage === "unlock") {
-  
-  try {
-    const hash = await approveTokens({
-      abi: token_abi,
-      address: token_address,
-      functionName: "approve",
-      args: [contract_address, parseUnits(tokenAmount.toString(), 18)],
-    });
-    const { data } = useWaitForTransactionReceipt({hash});
-      console.log(data)
-      await farmTokens({
-        abi: contract_abi,
-        address: contract_address,
-        functionName: "farm",
-        args: [parseUnits(tokenAmount.toString(), 18), 0],
-      });
-      alert("Staked successfully");
-    } catch (error) {
-      alert(error)
-    }
-}
-}
 async function readUserInformation(userInformation: Array<unknown>,setTotalStaked: React.Dispatch<React.SetStateAction<string>>) {
   if(userInformation){
     const TotalTokenStaked = (userInformation[0] as bigint[]).reduce((a: bigint, b: bigint) => a + b, 0n);
